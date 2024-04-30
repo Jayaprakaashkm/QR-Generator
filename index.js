c113beb326c8a8f833d1ffd3dd03fa01f21cfac1
Jayaprakaashkm <jayprakaash7@gmail.com>
2024-04-30 11:19:23 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import inquirer from 'inquirer';
+import qr from "qr-image";
+import fs from "fs";
+
+
+var svg_string = qr.imageSync('I love QR!', { type: 'svg' });
 
 inquirer
     .prompt([
-        /* Pass your questions in here */
+        {
+            "message": "Type in your URL:",
+            "name": "URL"
+        }
+
+
     ])
     .then((answers) => {
-        // Use user feedback for... whatever!!
+        const url = answers.URL;
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('qr_img.png'));
     })
     .catch((error) => {
         if (error.isTtyError) {
